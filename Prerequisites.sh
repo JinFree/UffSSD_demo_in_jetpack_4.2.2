4dfb3cf1f54f6814b75c57b8bee4d6ef30c69c00
JinFree <ytrqwe12@gmail.com>
2019-12-05 14:05:40 +0900

    first commit

---
+++ Prerequisites.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2017_11_17.tar.gz
+
+/usr/lib/python3.6/dist-packages/uff/bin
---
+++ README.md
@@ -0,0 +1,1 @@
+# UffSSD_demo_in_jetpack_4.2.2
