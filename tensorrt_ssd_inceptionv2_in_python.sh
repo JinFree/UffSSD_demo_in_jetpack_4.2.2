a273b3a9d8a58494e6120db032968ddf6762183c
JinFree <ytrqwe12@gmail.com>
2019-12-06 16:41:16 +0900

    test image and fix code

--- tensorrt_ssd_inceptionv2_in_python.sh
+++ tensorrt_ssd_inceptionv2_in_python.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pip3 install pycuda
-cd samples/python/uff_ssd/plugin
+cd samples/python/uff_ssd
 mkdir build && cd build && cmake .. && make && cd ..
-python3 detect_objects.py -p 32 -b 1 ../../../solidWhiteRight.jpg
-python3 detect_objects.py -p 16 -b 1 ../../../solidWhiteRight.jpg
+python3 detect_objects.py -p 32 -b 1 -o ../../../fp32.jpg ../../../image1.jpg
+python3 detect_objects.py -p 16 -b 1 -o ../../../fp16.jpg ../../../image1.jpg
